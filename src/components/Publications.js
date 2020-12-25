d89ef1c20fba53733fffb30e09d4b801c121125d
Doug Schallmoser <dougschallmoser@gmail.com>
2020-12-24 20:04:11 -0800

    make publication title the link to source

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -18,14 +18,14 @@ const Publications = () => {
       <div className="publication-container">
         <div className="publications-time">Fall 2020</div>
         <div className="publication">
-          "Unicorn Cake" / <em>Litro</em> / <a href="https://www.litromagazine.com/usa/2020/11/unicorn-cake/" target="_blank" rel="noreferrer">Read Story</a>
+        <a href="https://www.litromagazine.com/usa/2020/11/unicorn-cake/" target="_blank" rel="noreferrer">"Unicorn Cake"</a> / <em>Litro</em>
         </div>
       </div>
 
       <div className="publication-container">
         <div className="publications-time">Summer 2020</div>
         <div className="publication">
-          "The Crawdad" / <em>Flash Fiction Magazine</em> / <a href="https://flashfictionmagazine.com/?s=The+Crawdad" target="_blank" rel="noreferrer">Read Story</a>
+        <a href="https://flashfictionmagazine.com/?s=The+Crawdad" target="_blank" rel="noreferrer">"The Crawdad"</a> / <em>Flash Fiction Magazine</em>
         </div>
       </div>
 
