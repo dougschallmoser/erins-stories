4c991bed552bf19026c77cd4210fde39f742fff9
Doug Schallmoser <dougschallmoser@gmail.com>
2021-01-01 15:27:19 -0800

    add iframe for publication preview, add logic for displaying iframe

--- src/components/Publication.js
+++ src/components/Publication.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Publication = ({ link, title, source }) => {
+
+  const [displayPreview, setDisplayPreview] = useState(false)
+
+  const handleClick = () => {
+    setDisplayPreview(!displayPreview)
+  }
+
+  const handleClose = () => {
+    setDisplayPreview(false)
+  }
+
   return (
     <div className="publication">
       {link ? 
         <>
-          <a href={link} target="_blank" rel="noreferrer">"{title}"</a> / <em>{source}</em>
+          <span className="publication-title" onClick={handleClick}>"{title}"</span> / <em>{source}</em>
+          {displayPreview && 
+            <div className="box">
+              <div className="close" onClick={handleClose}>close preview</div>
+              <div className="open"><a href={link} target="_blank" rel="noreferrer">open story</a></div>
+              <iframe id="preview-frame" src={link} title="Preview Story" />
+            </div> 
+        }
         </>
         : 
         <>
