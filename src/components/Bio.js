05fc8c20af11a3be6e98518748ebda09afb64da3
Doug Schallmoser <dougschallmoser@gmail.com>
2021-01-07 14:35:16 -0800

    rename divs

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -3,10 +3,10 @@ import React from 'react';
 const Bio = () => {
   return (
     <div className="bio-container">
-      <div id="bio-left">
+      <div id="bio-photo">
         <img src="profile.jpg" alt="Erin's Profile" />
       </div>
-      <div id="bio-right">
+      <div id="bio-text">
         <p>Erin Schallmoser (she/her) lives in Bellingham, WA, works by day as a naturopathic clinic manager, and delights in moss, slugs, stones, wildflowers, small birds, and the moon, when she can see it. She's also a poetry/prose editor and staff contributor at The Aurora Journal and is currently working on various flash fiction, creative nonfiction, and poetry projects.</p>
       </div>
     </div>
