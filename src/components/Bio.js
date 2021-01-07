b9f062779187de959c7c92eae24c8edbb6c6b8d1
Doug Schallmoser <dougschallmoser@gmail.com>
2021-01-07 12:36:21 -0800

    build Bio component for rendering profile photo and bio text

---
+++ src/components/Bio.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Bio = () => {
+  return (
+    <div className="bio-container">
+      <div id="bio-left">
+        <img src="profile.jpg" alt="Erin's Profile" />
+      </div>
+      <div id="bio-right">
+        <p>Erin Schallmoser (she/her) lives in Bellingham, WA, works by day as a naturopathic clinic manager, and delights in moss, slugs, stones, wildflowers, small birds, and the moon, when she can see it. She's also a poetry/prose editor and staff contributor at The Aurora Journal and is currently working on various flash fiction, creative nonfiction, and poetry projects.</p>
+      </div>
+    </div>
+  )
+}
+
+export default Bio;
