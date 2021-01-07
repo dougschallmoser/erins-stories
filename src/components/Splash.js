409f4fd4989cfb597084043e1cba6c65b065cf58
Doug Schallmoser <dougschallmoser@gmail.com>
2021-01-07 12:36:30 -0800

    load Bio component

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Bio from './Bio';
 
 const Splash = () => {
   return (
     <section id="splash">
       <div id="name">Erin Schallmoser</div>
       <div id="subtitle">Writer</div>
-      <img src="profile.jpg" alt="Erin's Profile" />
+      <Bio />
     </section>
   )
 }
