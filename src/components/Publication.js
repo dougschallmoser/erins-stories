e726a835983098c08513b82bb5bf5185da21bc23
Doug Schallmoser <dougschallmoser@gmail.com>
2021-01-17 12:47:05 -0800

    create function to handle hovering of publication to conditionally change bg color

--- src/components/Publication.js
+++ src/components/Publication.js
@@ -12,22 +12,42 @@ const Publication = ({ link, title, source }) => {
     setDisplayPreview(false)
   }
 
+  const handleHover = (event) => {
+    let div = event.currentTarget;
+
+    if (!link && div.className === "publication") {
+      div.className = "publication-hover"
+    } else if (div.className === "publication") {
+      div.className = "publication-hover2"
+    } else {
+      div.className = "publication"
+    }
+  }
+
   return (
     <div
       className="publication"
       onClick={link ? handleClick : null}
-      style={{ cursor: link ? "pointer" : "default", background: displayPreview ? "#00b49628" : null }}
+      onMouseEnter={handleHover}
+      onMouseLeave={handleHover}
+      style={{
+        cursor: link ? "pointer" : "default",
+        background: displayPreview ? "#00b49628" : null
+      }}
     >
       {link ? 
         <>
-          <span className="publication-title">"{title}"</span> / <em>{source}</em>
+          <span className="publication-title">
+            "{title}"
+          </span> / <em>{source}</em>
+
           {displayPreview && 
             <div className="box">
               <div className="close" onClick={handleClose}>close preview</div>
               <div className="open"><a href={link} target="_blank" rel="noreferrer">open story</a></div>
               <iframe id="preview-frame" sandbox="allow-same-origin allow-scripts allow-popups allow-forms" src={link} title="Preview Story" />
             </div> 
-        }
+          }
         </>
         : 
         <>
