a3abcbe124a99127f541da02265e69f8821cdc14
Doug Schallmoser <dougschallmoser@gmail.com>
2023-03-06 20:16:47 -0800

    Update Bio.js
    Change hometown

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -6,7 +6,7 @@ const Bio = ({ setMain }) => {
       <img src="profile.jpg" id="bio-photo" alt="Erin's Profile" />
       <Contact />
       <div id="bio-text">
-        <p>Erin Schallmoser (she/her) lives in eastern Tennessee. She delights in moss, slugs, stones, wildflowers, small birds, and the moon, when she can see it. She's also a poetry/prose editor and staff contributor at The Aurora Journal, first reader at Pigeonholes, and editor-in-chief at <a href="https://gastropodalitmag.wixsite.com/main" target="_blank" rel="noreferrer">Gastropoda Lit Mag</a> and is currently working on various poetry projects.</p>
+        <p>Erin Schallmoser (she/her) lives in Port Angeles, Washington. She delights in moss, slugs, stones, wildflowers, small birds, and the moon, when she can see it. She's also a poetry/prose editor and staff contributor at The Aurora Journal, first reader at Pigeonholes, and editor-in-chief at <a href="https://gastropodalitmag.wixsite.com/main" target="_blank" rel="noreferrer">Gastropoda Lit Mag</a> and is currently working on various poetry projects.</p>
       </div>
     </div>
   )
